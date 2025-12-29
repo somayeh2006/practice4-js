5c2b157e419f287fd6c3bf6961f234b776a26741
Somayeh Gulestani <somayehgulistani14@gmail.com>
2025-12-29 23:53:18 +0430

    Add files via upload

---
+++ practice4.js
@@ -0,0 +1,20 @@
+//practice1
+
+let colors = ["red", "pink", "black", "white"];
+let result = colors.sort();
+let result2 = result.join();
+console.log(result);
+console.log(result2);
+
+
+
+//practice2
+
+let Shoppingcart =["pen", "vase", "Ruler"];
+let result1shop = Shoppingcart.includes("book");
+let result2shop = Shoppingcart.push("book");
+console.log(result2shop);
+console.log(Shoppingcart.length);
+
+
+
